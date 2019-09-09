e56480b7f606ee553a40c5453fe9fbb2830cca69
remicmacs <rembourgeon@gmail.com>
2019-09-09 14:59:21 +0200

    WiFi example with led indicator

--- wifi/src/main.cpp
+++ wifi/src/main.cpp
@@ -1,10 +1,33 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-// the setup function runs once when you press reset or power the board
+const char* ssid = "YameteRodolpheSan";
+const char* password =  "E5D4C3B2A1";
+
+const int LED = 2;
+
+void connectToNetwork() {
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Establishing connection to WiFi..");
+  }
+
+  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  Serial.println("Connected to network");
+
+}
+
 void setup() {
   Serial.begin(115200);
+  pinMode(LED, OUTPUT);
+
+  WiFi.scanNetworks();
+  connectToNetwork();
+
+  Serial.println(WiFi.macAddress());
+  Serial.println(WiFi.localIP());
 }
 
-void loop() {
-    Serial.println(F("Hello, world!"));
-}
+void loop() {}
