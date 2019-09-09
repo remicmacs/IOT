cdfa4b95252022db8ddd22dc3edb700887a74587
remicmacs <rembourgeon@gmail.com>
2019-09-09 18:54:53 +0200

    Completed WiFi server example

--- wifi/src/main.cpp
+++ wifi/src/main.cpp
@@ -1,9 +1,8 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include <inttypes.h>
 
 const char* ssid = "APESP32";
-const char* password =  "rootroot";
+const char* password = "rootroot";
 
 const int LED = 2;
 
@@ -12,42 +11,70 @@ WiFiServer Server(ServerPort);
 
 WiFiClient RemoteClient;
 
+/**
+ * Connection routine for telnet clients
+ */
 void checkForConnections()
 {
   if (Server.hasClient())
   {
-    // If we are already connected to another computer, 
+    // If we are already connected to another computer,
     // then reject the new connection. Otherwise accept
-    // the connection. 
+    // the connection.
     if (RemoteClient.connected())
     {
       Serial.println("Connection rejected");
       Server.available().stop();
-    }
-    else
-    {
+    } else {
       Serial.println("Connection accepted");
       RemoteClient = Server.available();
     }
   }
 }
 
+/**
+ * Client handling routine
+ */
 void listen() {
-  uint8_t ReceiveBuffer[30];
+  char * buffer;
+  uint8_t inputBuffer[30];
   while (RemoteClient.connected() && RemoteClient.available())
   {
-    int Received = RemoteClient.read(ReceiveBuffer, sizeof(ReceiveBuffer));
-    Serial.println((char *)ReceiveBuffer);
+    // Number of bytes received
+    int received = RemoteClient.read(inputBuffer, sizeof(inputBuffer));
 
-    if (strstr((char *) ReceiveBuffer, "On") != NULL) {
+    // Processing needed size of buffer
+    int needed = snprintf(NULL, 0, "Received %d bytes", received);
+    // Allocating buffer
+    buffer = (char *) malloc(needed + 1);
+
+    // Displaying buffer
+    sprintf(buffer, "Received %d bytes", received);
+    Serial.println(buffer);
+    Serial.println((char *)inputBuffer);
+
+    free(buffer);
+
+      // Allocating buffer
+      buffer = (char *) malloc(received-1);
+      strncpy(buffer, (char *) inputBuffer, received-2);
+      buffer[received-2] = '\0';
+
+    // Find if a command is found in the message
+    if (strcmp(buffer, "On") == 0) {
       Serial.println("Found On");
       digitalWrite(LED, HIGH);
-    } else if (strstr((char *) ReceiveBuffer, "Off") != NULL) {
+    } else if (strcmp(buffer, "Off") == 0) {
       Serial.println("Found Off");
       digitalWrite(LED, LOW);
+    } else {
+      Serial.print("Command not implemented yet : \"");
+      Serial.print(buffer);
+      Serial.println("\"");
     }
   }
 }
+
 void connectToNetwork() {
   WiFi.begin(ssid, password);
 
@@ -56,9 +83,10 @@ void connectToNetwork() {
     Serial.println("Establishing connection to WiFi..");
   }
 
+  // Blink three times if connection to WiFi is successful
   int blink = 3;
   while (blink-- > 0) {
-    digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+    digitalWrite(LED, HIGH);
     delay(100);
     digitalWrite(LED, LOW);
     delay(100);
